df150f8687ae5361b2d61c5e8c724b277bd75719
Robert Stoll <robert.stoll@tegonal.com>
2022-08-22 16:28:27 +0200

    prepare next dev cycle for v0.3.0

--- src/utils.sh
+++ src/utils.sh
@@ -5,7 +5,7 @@
 #  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Apache 2.0
 #  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
 #         /___/
-#                                         Version: v0.2.0
+#                                         Version: v0.3.0-SNAPSHOT
 #
 #######  Description  #############
 #
