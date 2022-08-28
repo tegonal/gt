eaf60892473de0edf7c17d24eaa72399aa94766f
Robert Stoll <robert.stoll@tegonal.com>
2022-08-28 15:54:23 +0200

    v0.3.1

--- src/utils.sh
+++ src/utils.sh
@@ -5,7 +5,7 @@
 #  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Apache 2.0
 #  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
 #         /___/
-#                                         Version: v0.4.0-SNAPSHOT
+#                                         Version: v0.3.1
 #
 #######  Description  #############
 #
