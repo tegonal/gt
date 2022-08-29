8cdf4638b97c0f92f65de631cb92ebeaddffab8f
Robert Stoll <robert.stoll@tegonal.com>
2022-08-29 08:46:33 +0200

    simplify install.sh further since we use bash now
    moreover:
    - rename repo to repoUrl
    - move parsing into main function

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ shopt -s inherit_errexit
 unset CDPATH
 
 function logError() {
-	local msg=$1
+	local -r msg=$1
 	shift || die "could not shift by 1"
 	# shellcheck disable=SC2059
 	printf >&2 "\033[0;31mERROR\033[0m: $msg\n" "$@"
@@ -25,17 +25,18 @@ function die() {
 }
 
 function logSuccess() {
-	local msg=$1
+	local -r msg=$1
 	shift || die "could not shift by 1"
 	# shellcheck disable=SC2059
 	printf "\033[0;32mSUCCESS\033[0m: $msg\n" "$@"
 }
 
 function checkCommandExists() {
-	local name=$1
-	file=$(command -v "$name") || die "%s is not installed (or not in PATH) %s" "$name" "${2:-""}"
-	if ! [[ -x $file ]]; then
-		die "%s is on the system at %s (according to command) but cannot be executed" "$name" "$file"
+	local -r name=$1
+	local sigFile
+	sigFile=$(command -v "$name") || returnDying "%s is not installed (or not in PATH) %s" "$name" "${2:-""}" || return $?
+	if ! [[ -x $sigFile ]]; then
+		returnDying "%s is on the system at %s (according to command) but is not executable. Consider to execute:\nsudo chmod +x %s" "$name" "$sigFile" "$sigFile" || return $?
 	fi
 }
 
@@ -46,7 +47,7 @@ function exitIfCommandDoesNotExist() {
 }
 
 function deleteDirChmod777() {
-	local dir=$1
+	local -r dir=$1
 	shift || die "could not shift by 1"
 	# e.g files in .git will be write-protected and we don't want sudo for this command
 	# yet, if it fails, then we ignore the problem and still try to delete the folder
@@ -56,11 +57,12 @@ function deleteDirChmod777() {
 
 exitIfCommandDoesNotExist "git"
 
-projectName="gget"
-repo="https://github.com/tegonal/$projectName"
-tmpDir="${TMPDIR:-${TMP:-/tmp}}/${projectName}_installation"
-gpgDir="$tmpDir/gpg"
-repoDir="$tmpDir/repo"
+declare projectName="gget"
+declare repoUrl="https://github.com/tegonal/$projectName"
+declare tmpDir
+tmpDir=$(mktemp -d -t gget-install-XXXXXXXXXX)
+declare gpgDir="$tmpDir/gpg"
+declare repoDir="$tmpDir/repo"
 
 function cleanup() {
 	# necessary because .git files are sometime 700 and would require sudo to delete
@@ -70,13 +72,13 @@ function cleanup() {
 }
 
 function install() {
-	tag=$1
-	installDir=$2
-	symbolicLink=$3
+	local -r tag=$1
+	local -r installDir=$2
+	local -r symbolicLink=$3
 	shift 3 || die "could not shift by 3"
 
-	versionRegex="^(v[0-9]+)\.([0-9]+)\.[0-9]+(-RC[0-9]+)?$"
-	if ! echo "$tag" | grep -Eq "$versionRegex" >/dev/null; then
+	local -r versionRegex="^(v[0-9]+)\.([0-9]+)\.[0-9]+(-RC[0-9]+)?$"
+	if ! grep -Eq "$versionRegex" >/dev/null <<<"$tag"; then
 		die "tag needs to follow the regex %s -- given %s" "$versionRegex" "$tag"
 	fi
 
@@ -92,7 +94,7 @@ function install() {
 
 	cd "$repoDir"
 	git init >/dev/null
-	git remote add origin "$repo" >/dev/null
+	git remote add origin "$repoUrl" >/dev/null
 	git fetch --depth=1 origin "$tag" >/dev/null
 	git checkout -b "$tag" FETCH_HEAD >/dev/null
 
@@ -100,25 +102,33 @@ function install() {
 
 	# we will check the chosen version against the current gpg key,
 	# i.e. only if the signatures of the chosen version are still valid against the current key we are happy
-	publicKey="$tmpDir/signing-key.public.asc"
+	local -r publicKey="$tmpDir/signing-key.public.asc"
 	wget -O- -q "https://raw.githubusercontent.com/tegonal/$projectName/main/.gget/signing-key.public.asc" >"$publicKey"
 
-	gpg --homedir "$gpgDir" --import "$publicKey"
-	gpg --homedir="$gpgDir" --list-sig
+	gpg --homedir "$gpgDir" --import "$publicKey" || die "could not import public key"
+	gpg --homedir="$gpgDir" --list-sig || true
 
-	find "$repoDir" -name "*.sig" -print0 |
-		xargs -0 -I {} sh -c "file=\"\$(echo '{}' | rev | cut -c5- | rev)\"; echo \"verifying \$file\"; output=\"\$(gpg --homedir='$gpgDir' --keyid-format LONG --verify \"\$file.sig\" \"\$file\" 2>&1)\"; if ! [ \"\$?\" -eq 0 ]; then printf \"verification failed for %s:\n%s\n\n\" \"\$file\" \"\$output\"; exit 2; fi" || die "verication failed, see above"
+	find "$repoDir" -name "*.sig" -print0 | while read -r -d $'\0' sigFile; do
+		local file=${sigFile::-4}
+		echo "verifying $file"
+		local output
+		if ! output="$(gpg --homedir="$gpgDir" --keyid-format LONG --verify "$sigFile" "$file" 2>&1)"; then
+			printf "verification failed for %s:\n%s\n\n" "$sigFile" "$output"
+			return 2
+		fi
+	done || die "verification failed, see above"
 
 	echo "Verification complete, note that we did not verify $projectName's dependencies"
 	echo ""
 
 	if [[ -d $installDir ]]; then
 		currentBranch=$(git --git-dir="$repoDir/.git" rev-parse --abbrev-ref HEAD || echo "<UNKNOWN, most likely manual installation>")
-		echo "Looks like $projectName was already installed in $installDir. Current tag in use is $currentBranch"
-		echo "going to replace the current installation with the new one"
+		echo "Looks like $projectName was already installed in $installDir"
+		printf "Current tag in use is \033[0;36m%s\033[0m\n" "$currentBranch"
+		printf "going to replace the current installation with this one (%s)\n" "$tag"
 		# necessary because .git files are sometime 700 and would require sudo to delete
 		deleteDirChmod777 "$installDir"
-		if [[ -n "$symbolicLink" ]]; then
+		if [[ -n $symbolicLink ]]; then
 			rm "$symbolicLink" >/dev/null 2>&1 || true
 		fi
 	fi
@@ -144,54 +154,57 @@ function install() {
 	fi
 }
 
-tag=""
-installDir=""
-symbolicLink=""
-
 function exitIfValueMissing() {
 	[[ -n "${2:-}" ]] || die "only %s provided but not a corresponding value" "$1"
 }
 
-while [[ $# -gt 0 ]]; do
-	case $1 in
-	-t | --tag)
-		exitIfValueMissing "$@"
-		tag=$2 && shift
-		;;
-	-d | --directory)
-		exitIfValueMissing "$@"
-		installDir=$2 && shift
-		;;
-	-ln)
-		exitIfValueMissing "$@"
-		symbolicLink=$2 && shift
-		;;
-	-*) die "unknown option $1" ;;
-	*) die "unknown argument $1" ;;
-	esac
-	shift
-done
+function main() {
+	local tag=""
+	local installDir=""
+	local symbolicLink=""
 
-if [[ -z $tag ]]; then
-	echo "determine latest tag of $repo"
-	tag=$(git ls-remote --refs --tags "$repo" |
-		cut --delimiter='/' --fields=3 |
-		sort --version-sort |
-		tail --lines=1)
-fi
-if [[ -z $installDir ]] && [[ -n $symbolicLink ]]; then
-	die "you can only specify a symbolic link if you specify a custom installation directory."
-fi
-if [[ -z $installDir ]]; then
-	prefix=$(readlink -m "$HOME/.local")
-	installDir="$prefix/lib/$projectName"
-	symbolicLink="$prefix/bin/$projectName"
-	echo "configuring default installation directory ($installDir) and symbolic link ($symbolicLink)"
-fi
-installDir=$(readlink -m "$installDir")
-# if symbolicLink is relative, then make it absolute using pwd
-if [[ $symbolicLink != /* ]]; then
-	symbolicLink="$(pwd)/$symbolicLink"
-fi
+	while [[ $# -gt 0 ]]; do
+		case $1 in
+		-t | --tag)
+			exitIfValueMissing "$@"
+			tag=$2 && shift
+			;;
+		-d | --directory)
+			exitIfValueMissing "$@"
+			installDir=$2 && shift
+			;;
+		-ln)
+			exitIfValueMissing "$@"
+			symbolicLink=$2 && shift
+			;;
+		-*) die "unknown option $1" ;;
+		*) die "unknown argument $1" ;;
+		esac
+		shift
+	done
 
-install "$tag" "$installDir" "$symbolicLink"
+	if [[ -z $tag ]]; then
+		echo "determine latest tag of $repoUrl"
+		tag=$(git ls-remote --refs --tags "$repoUrl" |
+			cut --delimiter='/' --fields=3 |
+			sort --version-sort |
+			tail --lines=1)
+	fi
+	if [[ -z $installDir && -n $symbolicLink ]]; then
+		die "you can only specify a symbolic link if you specify a custom installation directory."
+	fi
+	if [[ -z $installDir ]]; then
+		prefix=$(readlink -m "$HOME/.local")
+		installDir="$prefix/lib/$projectName"
+		symbolicLink="$prefix/bin/$projectName"
+		echo "using default installation directory ($installDir) and symbolic link ($symbolicLink), use --directory and --ln to specify custom values"
+	fi
+	installDir=$(readlink -m "$installDir")
+	# if symbolicLink is relative, then make it absolute using pwd
+	if [[ -n $symbolicLink && $symbolicLink != /* ]]; then
+		symbolicLink="$(pwd)/$symbolicLink"
+	fi
+
+	install "$tag" "$installDir" "$symbolicLink"
+}
+main "$@"
