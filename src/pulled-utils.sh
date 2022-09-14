c8be92c647f760f881b7541d03ef37aeb8a16545
Robert Stoll <robert.stoll@tegonal.com>
2022-09-14 15:48:20 +0200

    v0.6.0

--- src/pulled-utils.sh
+++ src/pulled-utils.sh
@@ -5,7 +5,7 @@
 #  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Apache 2.0
 #  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
 #         /___/
-#                                         Version: v0.6.0-SNAPSHOT
+#                                         Version: v0.6.0
 #
 #######  Description  #############
 #
@@ -16,7 +16,7 @@
 set -euo pipefail
 shopt -s inherit_errexit
 unset CDPATH
-export GGET_VERSION='v0.6.0-SNAPSHOT'
+export GGET_VERSION='v0.6.0'
 
 if ! [[ -v dir_of_gget ]]; then
 	dir_of_gget="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" >/dev/null && pwd 2>/dev/null)"
