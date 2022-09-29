671d6dc4ca2f61bbf589e61e90252e38009f2945
Robert Stoll <robert.stoll@tegonal.com>
2022-09-29 08:36:24 +0200

    prepare next dev cycle for v0.9.0

--- scripts/additional-prepare-files-next-dev-cycle-steps.sh
+++ scripts/additional-prepare-files-next-dev-cycle-steps.sh
@@ -5,7 +5,7 @@
 #  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Creative Commons Zero v1.0 Universal
 #  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
 #         /___/
-#                                         Version: v0.8.0
+#                                         Version: v0.9.0-SNAPSHOT
 #
 ###################################
 set -euo pipefail
--- src/paths.source.sh
+++ src/paths.source.sh
@@ -6,7 +6,7 @@
 #  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Apache 2.0
 #  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
 #         /___/
-#                                         Version: v0.8.0
+#                                         Version: v0.9.0-SNAPSHOT
 #
 #######  Description  #############
 #
