f801cc8752744e669408fe060733c01d8f115982
Robert Stoll <robert.stoll@tegonal.com>
2023-04-11 11:19:43 +0200

    check again that installation works before releasing

--- scripts/release.sh
+++ scripts/release.sh
@@ -29,10 +29,9 @@ fi
 sourceOnce "$dir_of_tegonal_scripts/releasing/release-files.sh"
 
 function release() {
-# reactivate again after release it fails due to the rename from gget to gt
-#	if ! wget -q -O- "https://api.github.com/repos/tegonal/gt/actions/workflows/installation.yml/runs?per_page=1&status=completed&branch=main" | grep '"conclusion": "success"' > /dev/null; then
-#		die "installation workflow failed, you should not release ;-)"
-#	fi
+	if ! wget -q -O- "https://api.github.com/repos/tegonal/gt/actions/workflows/installation.yml/runs?per_page=1&status=completed&branch=main" | grep '"conclusion": "success"' > /dev/null; then
+		die "installation workflow failed, you should not release ;-)"
+	fi
 
 	function findFilesToRelease() {
 		find "$projectDir/src" \
