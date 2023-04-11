bc9eb27c5553b7a62f2068e41c6ed5a37bbd60b6
Robert Stoll <robert.stoll@tegonal.com>
2023-04-11 10:42:02 +0200

    v0.10.0

--- .gt/remotes/tegonal-gh-commons/pull-hook.sh
+++ .gt/remotes/tegonal-gh-commons/pull-hook.sh
@@ -5,13 +5,13 @@
 #  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Apache License 2.0
 #  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
 #         /___/
-#                                         Version: v0.10.0-SNAPSHOT
+#                                         Version: v0.10.0
 #
 ###################################
 set -euo pipefail
 shopt -s inherit_errexit
 unset CDPATH
-GT_LATEST_VERSION="v0.9.0"
+GT_LATEST_VERSION="v0.10.0"
 
 if ! [[ -v dir_of_tegonal_scripts ]]; then
 	dir_of_tegonal_scripts="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" >/dev/null && pwd 2>/dev/null)/../../../lib/tegonal-scripts/src"
