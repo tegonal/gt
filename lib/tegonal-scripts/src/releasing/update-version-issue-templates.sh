87201cbdcb2dddc34ac1651ca77ac17e3e8f4d3f
Robert Stoll <robert.stoll@tegonal.com>
2023-07-17 23:15:04 +0200

    update version in bug template during release

---
+++ lib/tegonal-scripts/src/releasing/update-version-issue-templates.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+#
+#    __                          __
+#   / /____ ___ ____  ___  ___ _/ /       This script is provided to you by https://github.com/tegonal/scripts
+#  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Apache License 2.0
+#  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
+#         /___/
+#                                         Version: v1.1.0
+#
+#######  Description  #############
+#
+#  Updates the placeholder of all labels named `Affected Version` in issue templates.
+#
+#######  Usage  ###################
+#
+#    #!/usr/bin/env bash
+#    set -euo pipefail
+#    shopt -s inherit_errexit
+#    # Assumes tegonal's scripts were fetched with gt - adjust location accordingly
+#    dir_of_tegonal_scripts="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" >/dev/null && pwd 2>/dev/null)/../lib/tegonal-scripts/src"
+#    source "$dir_of_tegonal_scripts/setup.sh" "$dir_of_tegonal_scripts"
+#
+#    "$dir_of_tegonal_scripts/releasing/update-issue-templates.sh" -v 0.1.0
+#
+#    # if you use it in combination with other tegonal-scripts files, then you might want to source it instead
+#    sourceOnce "$dir_of_tegonal_scripts/releasing/update-issue-templates.sh"
+#
+#    # and then call the function
+#    updateVersionIssueTemplate -v 0.2.0
+#
+###################################
+set -euo pipefail
+shopt -s inherit_errexit
+unset CDPATH
+export TEGONAL_SCRIPTS_VERSION='v1.1.0'
+
+if ! [[ -v dir_of_tegonal_scripts ]]; then
+	dir_of_tegonal_scripts="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" >/dev/null && pwd 2>/dev/null)/.."
+	source "$dir_of_tegonal_scripts/setup.sh" "$dir_of_tegonal_scripts"
+fi
+sourceOnce "$dir_of_tegonal_scripts/utility/parse-args.sh"
+
+function updateVersionIssueTemplates() {
+	local version directory additionalPattern
+	# shellcheck disable=SC2034   # is passed to parseArguments by name
+	local -ra params=(
+		version '-v' 'the version which shall be used'
+		directory '-d|--directory' '(optional) the working directory in which *.y(a)ml are searched (also in subdirectories) / you can also specify a file -- default: ./.github/ISSUE_TEMPLATE'
+		additionalPattern '-p|--pattern' '(optional) pattern which is used in a perl command (separator /) to search & replace additional occurrences. It should define two match groups and the replace operation looks as follows: '"\\\${1}\$version\\\${2}"
+	)
+	local -r examples=$(
+		# shellcheck disable=SC2312
+		cat <<-EOM
+			# update version to v0.1.0 for all *.y(a)ml in ./.github/ISSUE_TEMPLATE and subdirectories
+			update-version-issue-templates.sh -v v0.1.0
+
+			# update version to v0.1.0 for all *.y(a)ml in ./tpls and subdirectories
+			update-version-issue-templates.sh -v v0.1.0 -d ./tpls
+
+			# update version to v0.1.0 for all *.y(a)ml in ./.github/ISSUE_TEMPLATE and subdirectories
+			# also replace occurrences of the defined pattern
+			update-version-issue-templates.sh -v v0.1.0 -p "(VERSION=['\"])[^'\"]+(['\"])"
+		EOM
+	)
+
+	parseArguments params "$examples" "$TEGONAL_SCRIPTS_VERSION" "$@"
+	if ! [[ -v directory ]]; then directory="./.github/ISSUE_TEMPLATE"; fi
+	if ! [[ -v additionalPattern ]]; then additionalPattern=""; fi
+	exitIfNotAllArgumentsSet params "$examples" "$TEGONAL_SCRIPTS_VERSION"
+
+	local where
+	if [[ -f $directory ]]; then
+		where="file $directory"
+	else
+		where="directory $directory (and subdirectories)"
+	fi
+	echo "set version $version in issue templates in $where"
+	if [[ -n $additionalPattern ]]; then
+		echo "also going to search for $additionalPattern and replace with \${1}$version\${2}"
+	fi
+
+	local script
+	find "$directory" '(' -name "*.yml" -o -name "*.yaml" ')' -print0 |
+		while read -r -d $'\0' script; do
+			perl -0777 -i \
+				-pe "s/(label:\s*Affected Version[\S\s]+placeholder:\s*)\"[^\"]+\"/\${1}\"$version\"/g;" \
+				"$script" || returnDying "was not able to update the version in the issue templates" || return $?
+
+			if [[ -n $additionalPattern ]]; then
+				perl -0777 -i \
+					-pe "s/$additionalPattern/\${1}$version\${2}/g;" \
+					"$script" || returnDying "error during the additional replacement, see above" || return $?
+			fi
+		done
+}
+${__SOURCED__:+return}
+updateVersionIssueTemplates "$@"
