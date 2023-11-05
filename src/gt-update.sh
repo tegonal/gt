26f9951b7061ca0cc333b717793b7050c6d172a3
Robert Stoll <robert.stoll@tegonal.com>
2023-11-05 23:17:27 +0100

    prepare next dev cycle for v0.14.0

--- src/gt-update.sh
+++ src/gt-update.sh
@@ -5,7 +5,7 @@
 #  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under European Union Public License 1.2
 #  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
 #         /___/
-#                                         Version: v0.13.3
+#                                         Version: v0.14.0-SNAPSHOT
 #
 #######  Description  #############
 #
@@ -28,7 +28,7 @@
 set -euo pipefail
 shopt -s inherit_errexit
 unset CDPATH
-export GT_VERSION='v0.13.3'
+export GT_VERSION='v0.14.0-SNAPSHOT'
 
 if ! [[ -v dir_of_gt ]]; then
 	dir_of_gt="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" >/dev/null && pwd 2>/dev/null)"
