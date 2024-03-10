7b38e18ac291ac1622b714fa56d610a7e7d23caa
Robert Stoll <robert.stoll@tegonal.com>
2024-03-10 22:24:10 +0100

    v0.17.0

--- .gt/remotes/tegonal-gh-commons/pull-hook.sh
+++ .gt/remotes/tegonal-gh-commons/pull-hook.sh
@@ -6,12 +6,12 @@
 #  \__/\__/\_, /\___/_//_/\_,_/_/         It is licensed under European Union Public License v. 1.2
 #         /___/                           Please report bugs and contribute back your improvements
 #
-#                                         Version: v0.17.0-SNAPSHOT
+#                                         Version: v0.17.0
 ###################################
 set -euo pipefail
 shopt -s inherit_errexit
 unset CDPATH
-GT_LATEST_VERSION="v0.16.0"
+GT_LATEST_VERSION="v0.17.0"
 
 if ! [[ -v dir_of_tegonal_scripts ]]; then
 	dir_of_tegonal_scripts="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" >/dev/null && pwd 2>/dev/null)/../../../lib/tegonal-scripts/src"
--- src/gt-update.sh
+++ src/gt-update.sh
@@ -6,7 +6,7 @@
 #  \__/\__/\_, /\___/_//_/\_,_/_/         It is licensed under European Union Public License v. 1.2
 #         /___/                           Please report bugs and contribute back your improvements
 #
-#                                         Version: v0.17.0-SNAPSHOT
+#                                         Version: v0.17.0
 #######  Description  #############
 #
 #  'update' command of gt: utility to update already pulled files
@@ -28,7 +28,7 @@
 set -euo pipefail
 shopt -s inherit_errexit
 unset CDPATH
-export GT_VERSION='v0.17.0-SNAPSHOT'
+export GT_VERSION='v0.17.0'
 
 if ! [[ -v dir_of_gt ]]; then
 	dir_of_gt="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" >/dev/null && pwd 2>/dev/null)"
