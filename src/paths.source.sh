95702b66a64208f3f82ed193e447abe0dc07c0e5
Robert Stoll <robert.stoll@tegonal.com>
2024-04-01 23:35:23 +0200

    v0.17.3

--- src/paths.source.sh
+++ src/paths.source.sh
@@ -7,7 +7,7 @@
 #  \__/\__/\_, /\___/_//_/\_,_/_/         It is licensed under European Union Public License v. 1.2
 #         /___/                           Please report bugs and contribute back your improvements
 #
-#                                         Version: v0.18.0-SNAPSHOT
+#                                         Version: v0.17.3
 #######  Description  #############
 #
 #  constants intended to be sourced into a function.
