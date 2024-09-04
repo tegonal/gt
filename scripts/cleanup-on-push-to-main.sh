8294635daabc88ee0be0895541b7958241414633
Robert Stoll <robert.stoll@tegonal.com>
2024-09-04 14:30:46 +0200

    remove unused signatures via function from tegonal-scripts

--- scripts/cleanup-on-push-to-main.sh
+++ scripts/cleanup-on-push-to-main.sh
@@ -27,6 +27,7 @@ if ! [[ -v dir_of_tegonal_scripts ]]; then
 	source "$dir_of_tegonal_scripts/setup.sh" "$dir_of_tegonal_scripts"
 fi
 
+sourceOnce "$dir_of_tegonal_scripts/utility/cleanups.sh"
 sourceOnce "$dir_of_tegonal_scripts/utility/log.sh"
 sourceOnce "$dir_of_tegonal_scripts/utility/replace-help-snippet.sh"
 sourceOnce "$dir_of_tegonal_scripts/utility/update-bash-docu.sh"
@@ -84,18 +85,7 @@ function cleanupOnPushToMain() {
 			"$file" || return $?
 	done || die "could not replace the install instructions"
 
-	find "$projectDir" \
-  	  -type f \
-  		-name "*.sig" \
-  		-not -path "$projectDir/.gt/signing-key.public.asc.sig" \
-  		-not -path "$projectDir/.gt/remotes/*/public-keys/*.sig" \
-  		-print0 |
-  		while read -r -d $'\0' sigFile; do
-  			if ! [[ -f ${sigFile::${#sigFile}-4} ]]; then
-						logInfo "remove unused signature \033[0;36m%s\033[0m as the corresponding file does no longer exist" "$sigFile"
-						rm "$sigFile"
-				fi
-			done
+	removeUnusedSignatures "$projectDir"
 
 	logSuccess "Cleanup on push to main completed"
 }
