01dce4484692e5bae358f99977e54465cabedca1
Robert Stoll <robert.stoll@tegonal.com>
2024-11-01 10:13:33 +0100

    prepare next dev cycle for v1.1.0

--- src/utils.sh
+++ src/utils.sh
@@ -6,7 +6,7 @@
 #  \__/\__/\_, /\___/_//_/\_,_/_/         It is licensed under European Union Public License v. 1.2
 #         /___/                           Please report bugs and contribute back your improvements
 #
-#                                         Version: v1.0.2
+#                                         Version: v1.1.0-SNAPSHOT
 #######  Description  #############
 #
 #  internal utility functions
