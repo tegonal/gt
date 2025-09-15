ebcb277eb05a2bd9015e6523cdb65226a3008f6d
Robert Stoll <robert.stoll@tegonal.com>
2025-09-15 13:44:12 +0200

    v1.4.4 fix reset for non english locale

--- install.doc.sh
+++ install.doc.sh
@@ -6,8 +6,8 @@ gpg --verify ./signing-key.public.asc.sig ./signing-key.public.asc && \
 echo "public key trusted" && \
 mkdir ./gpg && \
 gpg --homedir ./gpg --import ./signing-key.public.asc && \
-wget "https://raw.githubusercontent.com/tegonal/gt/v1.4.3/install.sh" && \
-wget "https://raw.githubusercontent.com/tegonal/gt/v1.4.3/install.sh.sig" && \
+wget "https://raw.githubusercontent.com/tegonal/gt/v1.4.4/install.sh" && \
+wget "https://raw.githubusercontent.com/tegonal/gt/v1.4.4/install.sh.sig" && \
 gpg --homedir ./gpg --verify ./install.sh.sig ./install.sh && \
 chmod +x ./install.sh && \
 echo "verification successful" || (printf >&2 "\033[0;31mERROR\033[0m: verification failed, don't continue !!\n"; exit 1) && \
