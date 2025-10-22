5599ba91e7a5812173a44043a721447b6b14c0a5
Robert Stoll <robert.stoll@tegonal.com>
2025-10-22 17:48:30 +0200

    v1.5.0 placeholders

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -1,4 +1,4 @@
 
 
 ______________________________________
-I confirm that I have read the [Contributor Agreement v1.1](https://github.com/tegonal/gt/blob/v1.4.4/.github/Contributor%20Agreement.txt), agree to be bound on them and confirm that my contribution is compliant.
+I confirm that I have read the [Contributor Agreement v1.1](https://github.com/tegonal/gt/blob/v1.5.0/.github/Contributor%20Agreement.txt), agree to be bound on them and confirm that my contribution is compliant.
--- src/pulled-utils.sh
+++ src/pulled-utils.sh
@@ -6,7 +6,7 @@
 #  \__/\__/\_, /\___/_//_/\_,_/_/         It is licensed under European Union Public License v. 1.2
 #         /___/                           Please report bugs and contribute back your improvements
 #
-#                                         Version: v1.5.0-SNAPSHOT
+#                                         Version: v1.5.0
 #######  Description  #############
 #
 #  internal utility functions for dealing with the pulled file
@@ -16,7 +16,7 @@
 set -euo pipefail
 shopt -s inherit_errexit || { echo >&2 "please update to bash 5, see errors above" && exit 1; }
 unset CDPATH
-export GT_VERSION='v1.5.0-SNAPSHOT'
+export GT_VERSION='v1.5.0'
 
 if ! [[ -v dir_of_gt ]]; then
 	dir_of_gt="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" >/dev/null && pwd 2>/dev/null)"
