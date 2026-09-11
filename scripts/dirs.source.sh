8a9518b30171a0a0d9489f3d43a80ed7761f54fa
Robert Stoll <robert.stoll@tegonal.com>
2026-09-11 23:02:05 +0200

    prepare next dev cycle for v2.1.0

--- scripts/dirs.source.sh
+++ scripts/dirs.source.sh
@@ -7,7 +7,7 @@
 #  \__/\__/\_, /\___/_//_/\_,_/_/         It is licensed under European Union Public License v. 1.2
 #         /___/                           Please report bugs and contribute back your improvements
 #
-#                                         Version: v2.0.3
+#                                         Version: v2.1.0-SNAPSHOT
 #######  Description  #############
 #
 #  defines constants pointing to source root directories intended to be sourced in sh files
